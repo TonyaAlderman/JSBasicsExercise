9fb5d51fc5f449e195e3f8406bb375a506ecf5cf
TonyaAlderman <talderm99@gmail.com>
2021-09-20 20:34:38 -0500

    Finished JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log(7);
+// Numbers, Strings, Booleans, Null, Undefined
+// + - * / ** %
+console.log(1%5);
+console.log(6%5);
+console.log('I\'m Mary Poppins, Ya\'ll');
+console.log("Hawk" + 'eye');
+console.log("tonya".length + "alderman".length);
+console.log("Avengers: Endgame"[16]);
+//BONUS
+console.log("\\");
+
+console.log((5**4)-9);
+
+console.log(("I".length + "AM".length)%("IRON".length)-("MAN".length));
+
+console.log("cap"[1]);
+console.log("vision"[0]);
+console.log("okoye"[4]);
+console.log("nick fury"[0]);
+console.log("groot"[0]);
+console.log("doctor strange"[13]);
+console.log("thor"[3]);
+console.log("star-lord"[0]);
+
+var m = 25
+var c = 4
+var u = (m+c)
+console.log(u);
